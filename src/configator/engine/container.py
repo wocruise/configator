85f81237af9c2eb59a176d5178864e1598448791
pnhung177 <pnhung177@gmail.com>
2020-12-06 20:58:16 +0700

    Enhance the SettingCapsule with the readwritelock

--- src/configator/engine/container.py
+++ src/configator/engine/container.py
@@ -2,7 +2,9 @@
 
 import logging
 import threading
+import time
 
+from readerwriterlock import rwlock
 from typing import Callable, Dict, Optional
 
 LOG = logging.getLogger(__name__)
@@ -10,15 +12,46 @@ LOG = logging.getLogger(__name__)
 class SettingCapsule():
     __name = None
     __load = None
-    __lock = threading.RLock()
-    __payload = None
+    __data = None
+    __on_reset = None
+    __on_pre_load = None
+    __on_load_error = None
+    __on_post_load = None
     #
     #
-    def __init__(self, name: str, load: Callable):
+    def __init__(self, name: str, load: Callable,
+            on_reset:Optional[Callable]=None,
+            on_pre_load:Optional[Callable]=None,
+            on_post_load:Optional[Callable]=None,
+            on_load_error:Optional[Callable]=None):
         assert isinstance(name, str) and name, "[name] must be a string and not empty"
         self.__name = name
+        #
         assert callable(load), "[load] must be a function"
         self.__load = load
+        #
+        if callable(on_reset):
+            self.__on_reset = on_reset
+        else:
+            self.__on_reset = default_on_reset
+        #
+        if callable(on_pre_load):
+            self.__on_pre_load = on_pre_load
+        else:
+            self.__on_pre_load = default_on_pre_load
+        #
+        if callable(on_post_load):
+            self.__on_post_load = on_post_load
+        else:
+            self.__on_post_load = default_on_post_load
+        #
+        if callable(on_load_error):
+            self.__on_load_error = on_load_error
+        else:
+            self.__on_load_error = default_on_load_error
+        #
+        self.__load_lock = threading.RLock()
+        self.__rwhandler = rwlock.RWLockFairD()
     #
     #
     @property
@@ -28,17 +61,49 @@ class SettingCapsule():
     #
     @property
     def data(self):
-        with self.__lock:
-            if self.__payload is None:
-                self.__payload = self.__load()
-                if LOG.isEnabledFor(logging.DEBUG):
-                    LOG.log(logging.DEBUG, 'Assign result of load() to the payload')
-            return self.__payload
+        with self.__rwhandler.gen_rlock():
+            if self.__data is None:
+                self.__reload()
+            return self.__data
     #
     #
     def reset(self, message: Optional[Dict] = None, err: Optional[Exception] = None):
-        with self.__lock:
-            self.__payload = None
-            if LOG.isEnabledFor(logging.DEBUG):
-                LOG.log(logging.DEBUG, 'payload has been reset')
+        with self.__rwhandler.gen_wlock():
+            self.__data = None
+            if callable(self.__on_reset):
+                self.__on_reset(self.__name, time.time())
         return self
+    #
+    #
+    def __reload(self):
+        with self.__load_lock:
+            if self.__data is None:
+                try:
+                    if callable(self.__on_pre_load):
+                        self.__on_pre_load(self.__name, time.time())
+                    #
+                    self.__data = self.__load()
+                    #
+                    if callable(self.__on_post_load):
+                        self.__on_post_load(self.__name, time.time())
+                except Exception as exception:
+                    if callable(self.__on_load_error):
+                        self.__on_load_error(self.__name, time.time(), exception)
+                    raise exception
+
+
+def default_on_reset(name, timestamp):
+    if LOG.isEnabledFor(logging.DEBUG):
+        LOG.log(logging.DEBUG, 'SettingCapsule[%s] reset the [data] to None', name)
+
+def default_on_pre_load(name, timestamp):
+    if LOG.isEnabledFor(logging.DEBUG):
+        LOG.log(logging.DEBUG, 'SettingCapsule[%s] invoke the load() to load the data', name)
+
+def default_on_post_load(name, timestamp):
+    if LOG.isEnabledFor(logging.DEBUG):
+        LOG.log(logging.DEBUG, 'SettingCapsule[%s] save result of load() to the store', name)
+
+def default_on_load_error(name, timestamp, error):
+    if LOG.isEnabledFor(logging.ERROR):
+        LOG.log(logging.ERROR, 'SettingCapsule[%s] error on call to load() function', name)
---
+++ tests/units/engine/container_test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from configator.engine import SettingCapsule
+from configator.utils.function import json_dumps
+
+
+class SettingCapsule__test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_ok(self):
+        controller = Controller()
+        #
+        total = 20
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            results = list()
+            for i in range(total):
+                if i == 3:
+                    futures.append(executor.submit(controller.refresh))
+                else:
+                    futures.append(executor.submit(controller.use_data))
+            for future in as_completed(futures):
+                results.append(future.result())
+        #
+        cat = dict()
+        for result in results:
+            if isinstance(result, dict):
+                if cat.get(result.get('timestamp')) is None:
+                    cat[result.get('timestamp')] = []
+                cat[result.get('timestamp')].append(result)
+        #
+        #
+        self.assertEqual(len(cat), 2)
+        parts = list(cat.values())
+        part0_len = len(parts[0])
+        part1_len = len(parts[1])
+        #
+        len_min = min(part0_len, part1_len)
+        len_max = max(part0_len, part1_len)
+        self.assertEqual(len_min, 3)
+        self.assertEqual(len_max, total - 1 - len_min)
+
+
+class Controller():
+    def __init__(self, *args, **kwargs):
+        self.__capsule = SettingCapsule(name='example', load=self.load)
+    #
+    def load(self):
+        return {
+            "timestamp": datetime.now().timestamp()
+        }
+    #
+    def use_data(self):
+        return self.__capsule.data
+    #
+    def refresh(self):
+        self.__capsule.reset()
+        return None
