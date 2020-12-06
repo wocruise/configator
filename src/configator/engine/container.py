8f34014ab8b3d9babbcf6f9c87e6666378fb3bfa
pnhung177 <pnhung177@gmail.com>
2020-12-07 02:59:44 +0700

    Enhance the SettingCapsule.reset() method

--- src/configator/engine/container.py
+++ src/configator/engine/container.py
@@ -13,27 +13,27 @@ class SettingCapsule():
     __name = None
     __load = None
     __data = None
-    __on_reset = None
+    __on_access = None
     __on_pre_load = None
-    __on_load_error = None
     __on_post_load = None
+    __on_load_error = None
+    __on_reset = None
     #
     #
     def __init__(self, name: str, load: Callable,
-            on_reset:Optional[Callable]=None,
+            on_access:Optional[Callable]=None,
             on_pre_load:Optional[Callable]=None,
             on_post_load:Optional[Callable]=None,
-            on_load_error:Optional[Callable]=None):
+            on_load_error:Optional[Callable]=None,
+            on_reset:Optional[Callable]=None):
         assert isinstance(name, str) and name, "[name] must be a string and not empty"
         self.__name = name
         #
         assert callable(load), "[load] must be a function"
         self.__load = load
         #
-        if callable(on_reset):
-            self.__on_reset = on_reset
-        else:
-            self.__on_reset = default_on_reset
+        if callable(on_access):
+            self.__on_access = on_access
         #
         if callable(on_pre_load):
             self.__on_pre_load = on_pre_load
@@ -50,6 +50,11 @@ class SettingCapsule():
         else:
             self.__on_load_error = default_on_load_error
         #
+        if callable(on_reset):
+            self.__on_reset = on_reset
+        else:
+            self.__on_reset = default_on_reset
+        #
         self.__load_lock = threading.RLock()
         self.__rwhandler = rwlock.RWLockFairD()
     #
@@ -62,34 +67,41 @@ class SettingCapsule():
     @property
     def data(self):
         with self.__rwhandler.gen_rlock():
+            if callable(self.__on_access):
+                self.__on_access(self.__name, time.time())
             if self.__data is None:
-                self.__reload()
+                with self.__load_lock:
+                    if self.__data is None:
+                        self.__data = self.__reload()
             return self.__data
     #
     #
-    def reset(self, message: Optional[Dict] = None, err: Optional[Exception] = None):
+    def reset(self, message: Optional[Dict] = None, err: Optional[Exception] = None, lazy_load=False, **kwargs):
         with self.__rwhandler.gen_wlock():
-            self.__data = None
             if callable(self.__on_reset):
                 self.__on_reset(self.__name, time.time())
+            if lazy_load:
+                self.__data = None
+            else:
+                self.__data = self.__reload()
         return self
     #
     #
     def __reload(self):
-        with self.__load_lock:
-            if self.__data is None:
-                try:
-                    if callable(self.__on_pre_load):
-                        self.__on_pre_load(self.__name, time.time())
-                    #
-                    self.__data = self.__load()
-                    #
-                    if callable(self.__on_post_load):
-                        self.__on_post_load(self.__name, time.time())
-                except Exception as exception:
-                    if callable(self.__on_load_error):
-                        self.__on_load_error(self.__name, time.time(), exception)
-                    raise exception
+        try:
+            if callable(self.__on_pre_load):
+                self.__on_pre_load(self.__name, time.time())
+            #
+            result = self.__load()
+            #
+            if callable(self.__on_post_load):
+                self.__on_post_load(self.__name, time.time())
+            #
+            return result
+        except Exception as exception:
+            if callable(self.__on_load_error):
+                self.__on_load_error(self.__name, time.time(), exception)
+            raise exception
 
 
 def default_on_reset(name, timestamp):
